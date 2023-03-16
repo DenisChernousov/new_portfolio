058e366460b711c65a4220677bd71ba71863ed2f
Denis Chernousov <chernousovdv@gmail.com>
2023-03-16 10:28:54 +0700

    start git

---
+++ src/assets/js/main.js
@@ -0,0 +1,38 @@
+
+document.querySelector('.burger').addEventListener('click', function () {
+    this.classList.toggle('active');
+    document.querySelector('.nav').classList.toggle('open');
+});
+
+
+
+window.onload = function () {
+    window.setInterval(function () {
+        let date = new Date();
+
+        let hours = date.getHours();
+        let minute = date.getMinutes();
+
+        let clock = hours + ":" + minute;
+
+        document.getElementById("clock").innerHTML = clock;
+    }
+
+    );
+}
+
+let text = `Могу, умею, практикую!
+HTML, CSS, JavaScript, WordPress, 
+React и что-то еще я же всетаки джун, обучусь :) `;
+
+let p = document.querySelector('.type-1');
+let text1 = '';
+let input = (i) => {
+    setTimeout(() => {
+        text1 += text[i];
+        p.textContent = text1;
+    }, 50 * i)
+}
+for (let i = 0; i < text.length - 1; i++) {
+    input(i);
+}
